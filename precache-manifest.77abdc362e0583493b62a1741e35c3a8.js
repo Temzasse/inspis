62c67da56898a28aba66b95379104bd9c55ce3c2
Temzasse <teemu.taskula@taitounited.fi>
2019-08-04 19:28:45 +0300

    Updates

---
+++ precache-manifest.77abdc362e0583493b62a1741e35c3a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27204f6525c53b31623e04108d40e2a8",
+    "url": "/inspis/index.html"
+  },
+  {
+    "revision": "d4d0157d8b4216f80df6",
+    "url": "/inspis/static/css/main.4ab4065f.chunk.css"
+  },
+  {
+    "revision": "a6a1ae50c670eac8285b",
+    "url": "/inspis/static/js/2.24638737.chunk.js"
+  },
+  {
+    "revision": "d4d0157d8b4216f80df6",
+    "url": "/inspis/static/js/main.f307c496.chunk.js"
+  },
+  {
+    "revision": "2817ba61f4a65f8cb24e",
+    "url": "/inspis/static/js/runtime~main.ba6fcbeb.js"
+  }
+]);
